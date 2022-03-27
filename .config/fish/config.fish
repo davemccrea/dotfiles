50072da2a0db56acb8fd3bf9fb494811f5dbc763
David McCrea <git@dmccrea.me>
2022-03-27 19:26:40 +0300

    Minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,4 @@ set -gx SUDO_EDITOR /home/david/.asdf/shims/nvim
 set -gx VISUAL nvim /home/david/.asdf/shims/nvim
 set -gx EDITOR nvim /home/david/.asdf/shims/nvim
 
+eval (dircolors ~/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
--- .config/nvim/plugins/airline.vim
+++ .config/nvim/plugins/airline.vim
@@ -2,7 +2,7 @@ Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
 
 let g:airline_theme = 'dracula'
-let g:airline_powerline_fonts = 1
+let g:airline_powerline_fonts = 0
 let g:airline_skip_empty_sections = 1
 
 let g:airline_left_sep = ''
