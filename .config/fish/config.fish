1306a3b99f947fdbdf187e6bc6a30534f44896d1
David McCrea <git@dmccrea.me>
2022-03-29 12:06:24 +0300

    Clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,3 @@ set -gx SUDO_EDITOR /home/david/.asdf/shims/nvim
 set -gx VISUAL nvim /home/david/.asdf/shims/nvim
 set -gx EDITOR nvim /home/david/.asdf/shims/nvim
 
-eval (dircolors ~/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
