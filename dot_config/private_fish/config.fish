94a9e4c598d5f9915cc42bee060eeddd7fe3f2ea
David McCrea <git@dmccrea.me>
2023-11-24 19:09:06 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,8 @@
 set -g fish_greeting
 
-if status is-interactive
+if status is-interactive 
 and not set -q TMUX
-    if tmux has-session -t home
-	exec tmux attach-session -t home
-    else
-        tmux new-session -s home
-    end
+    tmux attach -t base || tmux new -s base
 end
 
 source ~/.asdf/asdf.fish
