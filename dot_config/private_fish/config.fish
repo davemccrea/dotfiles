169593a89bed74f34c239604d2b5dc8eebed2e85
David McCrea <git@dmccrea.me>
2023-11-24 19:07:55 +0200

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,16 @@
 set -g fish_greeting
-set -gx EDITOR nvim
-set PATH ~/.local/bin $PATH
+
+if status is-interactive
+and not set -q TMUX
+    if tmux has-session -t home
+	exec tmux attach-session -t home
+    else
+        tmux new-session -s home
+    end
+end
+
 source ~/.asdf/asdf.fish
+
+set -gx EDITOR nvim
+
+set PATH ~/.local/bin $PATH
