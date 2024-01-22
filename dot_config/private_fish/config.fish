78df6aadc6159dc983dc53d4c5256583a1fc1908
David McCrea <git@dmccrea.me>
2024-01-22 11:22:37 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,10 @@
-set -g fish_greeting
 source /opt/asdf/asdf.fish
+zoxide init fish | source
+
+set -g fish_greeting
 set -x ASDF_DATA_DIR /opt/asdf
 set -x EDITOR nvim
 set PATH ~/.local/bin $PATH
+
 alias vi="nvim"
 alias vim="nvim"
