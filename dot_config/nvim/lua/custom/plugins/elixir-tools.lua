63872dad2a8681bb8c15cfbd01fdda65f12e5d8d
David McCrea <git@dmccrea.me>
2024-02-22 11:55:02 +0200

    Update .config/nvim/lua/custom/plugins/elixir-tools.lua

--- dot_config/nvim/lua/custom/plugins/elixir-tools.lua
+++ dot_config/nvim/lua/custom/plugins/elixir-tools.lua
@@ -7,19 +7,27 @@ return {
     local elixirls = require("elixir.elixirls")
 
     elixir.setup {
-      nextls = {enable = true},
-      credo = {},
+      nextls = {
+        enable = false,          -- defaults to false
+      },
+      credo = {
+        enable = true,                         -- defaults to true
+      },
       elixirls = {
-        enable = true,
+        -- specify a repository and branch
+        repo = "mhanberg/elixir-ls",         -- defaults to elixir-lsp/elixir-ls
+        branch = "mh/all-workspace-symbols", -- defaults to nil, just checkouts out the default branch, mutually exclusive with the `tag` option
+        tag = "v0.14.6",                     -- defaults to nil, mutually exclusive with the `branch` option
+
+        -- default settings, use the `settings` function to override settings
         settings = elixirls.settings {
-          dialyzerEnabled = false,
+          dialyzerEnabled = true,
+          fetchDeps = false,
           enableTestLenses = false,
+          suggestSpecs = false,
         },
         on_attach = function(client, bufnr)
-          vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-          vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-        end,
+        end
       }
     }
   end,
