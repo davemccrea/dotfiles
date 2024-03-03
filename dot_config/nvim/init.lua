2cfbbeb1787486841b54d2dce177457228c974ca
David McCrea <git@dmccrea.me>
2024-03-03 15:35:50 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -655,7 +655,6 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-{
   'olivercederborg/poimandres.nvim',
   lazy = false,
   priority = 1000,
