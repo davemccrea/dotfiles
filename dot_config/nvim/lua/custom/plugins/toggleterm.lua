8eeb068a38cb36fa356e8e1ebc04d9feab73d79d
David McCrea <git@dmccrea.me>
2024-03-05 18:23:53 +0200

    Update .config/nvim/lua/custom/plugins/toggleterm.lua

--- dot_config/nvim/lua/custom/plugins/toggleterm.lua
+++ dot_config/nvim/lua/custom/plugins/toggleterm.lua
@@ -1,9 +1,12 @@
 return {
-  {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    config = function()
-      require('toggleterm').setup()
-    end,
-  },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				shell = vim.o.shell,
+				direction = "float",
+			})
+		end,
+	},
 }
