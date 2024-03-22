1300ce1ed4f68473d26f9332c18fd89b38053309
David McCrea <git@dmccrea.me>
2024-03-22 21:21:06 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -668,6 +668,10 @@ require('lazy').setup({
     as = 'rose-pine',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      require('rose-pine').setup {
+        variant = 'moon',
+      }
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
