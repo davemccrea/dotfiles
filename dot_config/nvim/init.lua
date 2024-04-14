1c9f994bc23d95b369228c7ca3d38767f9661185
David McCrea <git@dmccrea.me>
2024-04-14 23:41:24 +0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -773,7 +773,7 @@ require("lazy").setup({
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		opts = {
-			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+			ensure_installed = { "bash", "c", "html", "lua", "markdown", "vim", "vimdoc", "elixir", "heex", "eex" },
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
