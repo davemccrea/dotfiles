6c9fb680f99d9efb2290b8287b5b75a520c85c84
David McCrea <git@dmccrea.me>
2024-07-06 11:06:45 +0300

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -774,7 +774,22 @@ require("lazy").setup({
 		"stevearc/oil.nvim",
 		opts = {},
 		config = function()
-			require("oil").setup()
+			require("oil").setup({
+				columns = { "icons" },
+				keymaps = {
+					["<C-h"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+
+				-- Open parent directory in current window
+				vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
+
+				-- Open parent directory in floating window
+				vim.keymap.set("n", "<space>-", require("oil").toggle_float),
+			})
 		end,
 		-- Optional dependencies
 		dependencies = { "echasnovski/mini.icons" },
