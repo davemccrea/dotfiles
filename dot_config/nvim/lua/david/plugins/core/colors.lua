67dd6bf38e9e329c017d81b80ccf0e4ff24496aa
David McCrea <git@dmccrea.me>
2024-07-10 18:15:24 +0300

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/david/plugins/core/colors.lua Add .config/nvim/lua/david/plugins/harpoon.lua

--- dot_config/nvim/lua/david/plugins/core/colors.lua
+++ dot_config/nvim/lua/david/plugins/core/colors.lua
@@ -5,7 +5,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.g.gruvbox_material_background = "soft"
+			vim.g.gruvbox_material_background = "dark"
 			vim.g.gruvbox_material_enable_italic = true
 
 			vim.cmd.colorscheme("gruvbox-material")
