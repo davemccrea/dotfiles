3e26165f9a4a6fd59fa1f683c28325458e462893
David McCrea <git@dmccrea.me>
2024-07-22 21:24:38 +0100

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,7 +6,7 @@ set -x _ZO_DATA_DIR /var/home/david/.local/share
 
 alias vi="nvim"
 alias lg="lazygit"
-alias ls="exa"
+alias ls="eza"
 alias tm="tmux new-session -A -s main"
 alias cdd="cd /var/home/david"
 alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
