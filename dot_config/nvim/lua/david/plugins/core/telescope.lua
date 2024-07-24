af4d94d8a00898280ec29178a57c57483c9b8d13
David McCrea <git@dmccrea.me>
2024-07-24 22:16:35 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/david/plugins/core/telescope.lua

--- dot_config/nvim/lua/david/plugins/core/telescope.lua
+++ dot_config/nvim/lua/david/plugins/core/telescope.lua
@@ -70,8 +70,12 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Search files" })
+		vim.keymap.set(
+			"n",
+			"<leader>sf",
+			"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+			{ desc = "[S]earch [F]iles" }
+		)
 		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
@@ -79,7 +83,6 @@ return {
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[Search] existing [B]uffers" })
-		vim.keymap.set("n", "<C-b>", builtin.buffers, { desc = "[Search] existing [B]uffers" })
 
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>/", function()
