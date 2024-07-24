7d166161feb49371997c759306ee372d5c6013aa
David McCrea <git@dmccrea.me>
2024-07-24 08:45:31 +0100

    Update .bashrc Update .config/nvim/lua/david/plugins/core/nvim-lspconfig.lua

--- dot_config/nvim/lua/david/plugins/core/nvim-lspconfig.lua
+++ dot_config/nvim/lua/david/plugins/core/nvim-lspconfig.lua
@@ -157,6 +157,7 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
+			elixirls = {},
 			-- clangd = {},
 			-- gopls = {},
 			-- pyright = {},
@@ -214,31 +215,5 @@ return {
 				end,
 			},
 		})
-
-		-- Lexical setup
-		local lspconfig = require("lspconfig")
-		local configs = require("lspconfig.configs")
-
-		local lexical_config = {
-			filetypes = { "elixir", "eelixir", "heex" },
-			cmd = { "/my/home/projects/_build/dev/package/lexical/bin/start_lexical.sh" },
-			settings = {},
-		}
-
-		if not configs.lexical then
-			configs.lexical = {
-				default_config = {
-					filetypes = lexical_config.filetypes,
-					cmd = lexical_config.cmd,
-					root_dir = function(fname)
-						return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
-					end,
-					-- optional settings
-					settings = lexical_config.settings,
-				},
-			}
-		end
-
-		lspconfig.lexical.setup({})
 	end,
 }
