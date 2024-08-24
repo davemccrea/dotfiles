39246196181bbec13986b7fdf4bd5d88fadde278
David McCrea <git@dmccrea.me>
2024-08-24 23:30:17 +0300

    Add .config/fish/completions/fisher.fish Add .config/fish/conf.d/atuin.env.fish Add .config/fish/conf.d/hydro.fish Add .config/fish/conf.d/rustup.fish Add .config/fish/config.fish Add .config/fish/fish_plugins Add .config/fish/fish_variables Add .config/fish/functions/cd.fish Add .config/fish/functions/fish_mode_prompt.fish Add .config/fish/functions/fish_prompt.fish Add .config/fish/functions/fish_prompt.pure-backup.fish Add .config/fish/functions/fish_title.pure-backup.fish Add .config/fish/functions/fisher.fish Add .config/fish/functions/gwip.fish Add .config/fish/functions/osversion.fish Add .config/fish/functions/theme_gruvbox.fish Add .config/fish/functions/tk.fish Add .config/fish/functions/wip.fish Add "dot_config/fish/themes/Ros\303\251 Pine Dawn.theme" Add "dot_config/fish/themes/Ros\303\251 Pine Moon.theme" Add "dot_config/fish/themes/Ros\303\251 Pine.theme"

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,28 @@
+set -g fish_greeting
+set -x EDITOR nvim
+set -x PLUG_EDITOR "vscode://file/__FILE__:__LINE__"
+set -x _ZO_DATA_DIR /var/home/david/.local/share
+
+alias vi="nvim"
+alias lg="lazygit"
+alias ls="eza --icons=auto"
+alias cdd="cd /var/home/david"
+
+#theme_gruvbox dark medium
+#fish_config theme choose "Rosé Pine Moon"
+fish_add_path ~/.composer/vendor/bin
+fish_add_path ~/.local/bin
+#fish_add_path ~/.local/bin/elixir-ls
+fish_add_path ~/go/bin
+fish_add_path /usr/local/go/bin
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.cache/rebar3/bin
+
+source ~/.asdf/asdf.fish
+source ~/.cargo/env.fish
+
+if status is-interactive
+	zoxide init fish | source
+	atuin init fish --disable-up-arrow | source
+end
+
