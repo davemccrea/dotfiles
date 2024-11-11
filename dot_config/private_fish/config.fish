ef8d46950a384021ee06d5b97cd7190996c7b85d
David McCrea <git@dmccrea.me>
2024-11-11 00:10:21 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -19,10 +19,10 @@ fish_add_path ~/.composer/vendor/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/go/bin
-fish_add_path ~/.cargo/bin
+#fish_add_path ~/.cargo/bin
 fish_add_path ~/.cache/rebar3/bin
 fish_add_path /opt/nvim-linux64/bin
 fish_add_path ~/bin
 
 source ~/.asdf/asdf.fish
-# source ~/.cargo/env.fish
+#source ~/.cargo/env.fish
