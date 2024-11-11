9c84d79661537aa550674adf299def9c22720a67
David McCrea <git@dmccrea.me>
2024-11-11 01:02:56 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -18,10 +18,10 @@ alias cdd="cd /var/home/david"
 fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/go/bin
-#fish_add_path ~/.cargo/bin
+fish_add_path ~/.cargo/bin
 fish_add_path ~/.cache/rebar3/bin
 fish_add_path /opt/nvim-linux64/bin
 fish_add_path ~/bin
 
 source ~/.asdf/asdf.fish
-#source ~/.cargo/env.fish
+source ~/.cargo/env.fish
