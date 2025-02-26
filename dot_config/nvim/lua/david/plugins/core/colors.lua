a2ceea1be15f357af943d4c3ae5d8c5a69a9b4d9
David McCrea <git@dmccrea.me>
2025-02-26 23:29:23 +0200

    Update .config/nvim/lua/david/plugins/core/colors.lua

--- dot_config/nvim/lua/david/plugins/core/colors.lua
+++ dot_config/nvim/lua/david/plugins/core/colors.lua
@@ -11,30 +11,4 @@ return {
 			vim.cmd.colorscheme("gruvbox-material")
 		end,
 	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				variant = "auto", -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
-				enable = {
-					terminal = true,
-				},
-				styles = {
-					bold = true,
-					italic = true,
-					transparency = false,
-				},
-			})
-
-			-- vim.cmd("colorscheme rose-pine")
-			-- vim.cmd("colorscheme rose-pine-main")
-			-- vim.cmd("colorscheme rose-pine-moon")
-			-- vim.cmd("colorscheme rose-pine-dawn")
-		end,
-	},
 }
