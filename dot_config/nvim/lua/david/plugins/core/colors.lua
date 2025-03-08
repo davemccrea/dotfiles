5ef1a1b077c868c8957d059274f739b7a92751bd
David McCrea <git@dmccrea.me>
2025-03-09 01:04:29 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/david/plugins/core/colors.lua

--- dot_config/nvim/lua/david/plugins/core/colors.lua
+++ dot_config/nvim/lua/david/plugins/core/colors.lua
@@ -1,14 +1,33 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			vim.g.gruvbox_material_foreground = "material"
-			vim.g.gruvbox_material_background = "medium"
-			vim.g.gruvbox_material_enable_italic = true
+			require("github-theme").setup({
+				-- ...
+			})
 
-			vim.cmd.colorscheme("gruvbox-material")
+			vim.cmd("colorscheme github_dark")
 		end,
 	},
+	-- {
+	-- 	"Mofiqul/dracula.nvim",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("dracula")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"sainnhe/gruvbox-material",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.g.gruvbox_material_foreground = "material"
+	-- 		vim.g.gruvbox_material_background = "medium"
+	-- 		vim.g.gruvbox_material_enable_italic = true
+	--
+	-- 		vim.cmd.colorscheme("gruvbox-material")
+	-- 	end,
+	-- },
 }
