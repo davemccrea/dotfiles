75d467e966c95ab577f7ade70222bb277423b839
David McCrea <git@dmccrea.me>
2025-03-09 00:37:27 +0200

    Add .config/fish/conf.d/02-aliases.fish Update .config/fish/conf.d/05-abbreviations.fish Remove .config/fish/conf.d/02-aliases.fish

--- dot_config/private_fish/conf.d/05-abbreviations.fish
+++ dot_config/private_fish/conf.d/05-abbreviations.fish
@@ -1,9 +1,8 @@
 if status is-interactive
-    abbr -a g git
+    abbr -a g "git"
     abbr -a gc "git commit"
     abbr -a gco "git checkout"
     abbr -a gs "git status"
-    abbr -a vi "nvim"
-    abbr -a lg "lazygit"
     abbr -a cdd "cd /var/home/david"
+    abbr -a dc "docker compose"
 end
