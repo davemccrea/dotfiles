1f426282925a4da336764d31c54aa556f46f0372
David McCrea <git@dmccrea.me>
2025-03-09 00:09:04 +0200

    Update .config/fish/conf.d/distrobox_config.fish

--- dot_config/private_fish/conf.d/distrobox_config.fish
+++ dot_config/private_fish/conf.d/distrobox_config.fish
@@ -1,3 +1,4 @@
+# https://github.com/89luca89/distrobox/issues/1671
 if test -e /etc/fish/conf.d/distrobox_config.fish
 	source /etc/fish/conf.d/distrobox_config.fish
 	source $XDG_CONFIG_HOME/fish/conf.d/fish_prompt.fish
