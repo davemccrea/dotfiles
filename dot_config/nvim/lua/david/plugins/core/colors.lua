dc088e9cc69d864c93b4a3ec8805f28a8aa608ee
David McCrea <git@dmccrea.me>
2025-03-09 02:13:07 +0200

    Update .config/nvim/lua/david/plugins/core/colors.lua

--- dot_config/nvim/lua/david/plugins/core/colors.lua
+++ dot_config/nvim/lua/david/plugins/core/colors.lua
@@ -4,9 +4,10 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		opts = {
-			transparent_background = true,
-		},
 		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
 			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
