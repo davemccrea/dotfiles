222dd4bcaeb98f4f96bb89969f2e8a0cc2fdc980
David McCrea <git@dmccrea.me>
2025-03-09 02:09:30 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/david/plugins/core/colors.lua

--- dot_config/nvim/lua/david/plugins/core/colors.lua
+++ dot_config/nvim/lua/david/plugins/core/colors.lua
@@ -1,17 +1,28 @@
 return {
 	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			require("github-theme").setup({
-				-- ...
+			vim.cmd.colorscheme("catppuccin")
+			require("catppuccin").setup({
+				transparent_background = false,
 			})
-
-			vim.cmd("colorscheme github_dark")
 		end,
 	},
+	-- {
+	-- 	"projekt0n/github-nvim-theme",
+	-- 	name = "github-theme",
+	-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		require("github-theme").setup({
+	-- 			-- ...
+	-- 		})
+	--
+	-- 		vim.cmd("colorscheme github_dark")
+	-- 	end,
+	-- },
 	-- {
 	-- 	"Mofiqul/dracula.nvim",
 	-- 	config = function()
