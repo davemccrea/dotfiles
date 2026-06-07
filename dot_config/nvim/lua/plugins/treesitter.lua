2064754babde8bd18cb47b41389f987467306175
David McCrea <mail@dmccrea.me>
2026-06-07 23:22:23 +0300

    update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" },
         auto_install = true,
         highlight = { enable = true, additional_vim_regex_highlighting = { "ruby" } },
         indent = { enable = true, disable = { "ruby" } },
