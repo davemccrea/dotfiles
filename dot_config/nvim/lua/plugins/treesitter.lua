2a98adfd01e2f1d04c98fc6ffe1e3dd495f81575
David McCrea <mail@dmccrea.me>
2026-06-13 22:27:40 +0300

    tidy

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,23 @@
+local ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" }
+
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "vim", "vimdoc" },
-        auto_install = true,
-        highlight = { enable = true, additional_vim_regex_highlighting = { "ruby" } },
-        indent = { enable = true, disable = { "ruby" } },
-    },
-    config = function(_, opts)
+    lazy = false,
+    config = function()
         require("nvim-treesitter.install").prefer_git = true
-        require("nvim-treesitter.configs").setup(opts)
+        require("nvim-treesitter").setup()
+        require("nvim-treesitter").install(ensure_installed)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function(args)
+                if vim.bo[args.buf].filetype == "ruby" then
+                    return
+                end
+
+                pcall(vim.treesitter.start, args.buf)
+            end,
+        })
     end,
 }
